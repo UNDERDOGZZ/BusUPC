831ac37eb66d45f3cd5bee5a57b988f182246078
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2018-11-18 21:43:28 -0500

    exq

--- Demo/Foro/FrontEnd/FAQUPC/src/app/model/likepregunta.ts
+++
@@ -1,9 +0,0 @@
-import { Pregunta } from "./pregunta";
-import { Usuario } from "./usuario";
-
-export class Likepregunta {
-    id:number;
-    questionId: Pregunta;
-    studentId: Usuario;
-    liked: boolean; 
-}
